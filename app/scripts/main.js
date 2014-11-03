e7085faa8ca24859901bdf01e022e615f7e648ef
Chase Simpson <chase.w.simpson1@gmail.com>
2014-11-03 17:22:12 -0500

    sass updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,20 @@
 var fav_movie = new Movies();
 
+$('#movie-click').on('submit', function (event) {
+  event.preventDefault();
+  var film = new Movie({
+    title: $('#title').val(),
+    director: $('#director').val()
+  });
+  console.log(film);
+  fav_movie.add(film);
+  film.save();
+  $(this)[0].reset();
+
+});
+
 fav_movie.fetch().done(function () {
   var movieview = new MovieView({
     collection: fav_movie
   });
 });
-
-// $('#form').on('submit', function (event) {
-//   event.preventDefault();
-//
-//   var movie_title = $('#name').val();
-//
-//   var film = new Movie({
-//     name: movie_title
-//   });
-//
-//   fav_movie.add(film);
-//
-//   film.save();
-//
-//   $(this)[0].reset();
-//
-// });
