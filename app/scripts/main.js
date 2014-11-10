56209ef8b1dde76918c02b9d08ebfc394ebda345
Chase Simpson <chase.w.simpson1@gmail.com>
2014-11-09 19:44:38 -0500

    onclick function added for sorting list

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,5 +10,31 @@
 
   });
 
+  $(document).ready(function(){
+    $("#rating-sort").click( function() {
+      $(this).css("color", "white");
+      $(this).css("text-shadow", "0 0 1px HSLA(215, 55%, 54%, 1)");
+      $('#genre-sort').css("color", "HSLA(215, 55%, 54%, 1)");
+      $('#title-sort').css("color", "HSLA(215, 55%, 54%, 1)");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#genre-sort").click( function() {
+      $(this).css("color", "white");
+      $(this).css("text-shadow", "0 0 1px HSLA(215, 55%, 54%, 1)");
+      $('#rating-sort').css("color", "HSLA(215, 55%, 54%, 1)");
+      $('#title-sort').css("color", "HSLA(215, 55%, 54%, 1)");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#title-sort").click( function() {
+      $(this).css("color", "white");
+      $(this).css("text-shadow", "0 0 1px HSLA(215, 55%, 54%, 1)");
+      $('#rating-sort').css("color", "HSLA(215, 55%, 54%, 1)");
+      $('#genre-sort').css("color", "HSLA(215, 55%, 54%, 1)");
+    });
+  });
 
 }());
